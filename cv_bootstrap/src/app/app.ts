c952788f1d2d2e94599edc28eccbd381e1bd7714
BARRY <bala.professionnel01@gmail.com>
2025-12-28 10:02:37 +0000

    Creer un nouveau composant entete

--- cv_bootstrap/src/app/app.ts
+++ cv_bootstrap/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Entete } from "./entete/entete";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Entete],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
